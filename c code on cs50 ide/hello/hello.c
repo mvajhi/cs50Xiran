b5d86f069e99fba2fff6d733efde176a671250bf
mahdi.vajhi <mvajhimv@gmail.com>
2022-11-03 13:36:06 +0330

    update

--- c code on cs50 ide/hello/hello.c
+++ c code on cs50 ide/hello/hello.c
@@ -4,7 +4,9 @@ int main(void)
 {
     //get name
     printf("what is your name? ");
-    char* name = scanf();
+    char name[10];
+    scanf("%s", name);
     //show name
     printf("hello, %s\n", name);
+    return 0;
 }
